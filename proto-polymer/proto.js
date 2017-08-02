83c2ebb2fee018cb39aa79fa1e813fcfc02fe4a5
Martin Jurča <mjurca@centrum.cz>
2017-08-02 19:04:46 +0200

    added a polymer 2.0 tech prototype

---
+++ proto-polymer/proto.js
@@ -0,0 +1,27 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement
+(function () {
+	'use strict'
+
+	document.createElement('szn-select');
+
+	document.onreadystatechange = function () {
+		if (document.readyState === 'interactive') {
+			init()
+		}
+	}
+
+	function init() {
+		var sznSelects = document.querySelectorAll('szn-select')
+		for (var i = 0, length = sznSelects.length; i < length; i++) {
+			initElement(sznSelects[i])
+		}
+	}
+
+	function initElement(sznSelect) {
+		var ui = sznSelect.querySelector('[data-szn-select-element="ui"]')
+		ui.innerHTML = '<div data-szn-select-element="duh">Are you there?\n</div>'
+		sznSelect.setAttribute('data-szn-select-ready', '')
+	}
+
+	window.initSznSelect = initElement;
+}())
